48cd5c8f015eb1dbb97ab0535751982fcbb5b229
Hsoren31 <sorensenhaley@gmail.com>
2025-07-02 08:05:16 -0600

    DEPLOYMENT: add previous properties back to pool

--- db/pool.js
+++ db/pool.js
@@ -1,6 +1,11 @@
 const { Pool } = require("pg");
 
 module.exports = new Pool({
+  host: process.env.PGHOST,
+  user: process.env.PGUSER,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT,
   connectionString: process.env.CONNECTION_STRING,
   ssl: true,
 });
